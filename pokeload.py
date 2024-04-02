c1b2b928eeb90c0ecc1a05d7f531cd878c727a6b
hugopzz30 <hugo.peror.04@gmail.com>
2024-04-02 00:06:08 -0600

    Clase 1

--- pokeload.py
+++ pokeload.py
@@ -13,4 +13,36 @@ pokemon_base = {
     "current_exp" : 0
 }
 
+def url():
+    url_list = []
+    for pokemon_page in range(1, 7):
+        url = "{}{}".format(URL_BASE, pokemon_page)
+        url_list.append(url)
+    return url_list
 
+
+def session():
+    session = HTMLSession()
+    return session
+
+
+def get_pokemon(url_list):
+    pokemons_dictionary = []
+    for pokemons in url_list:
+        pokemon = pokemon_base.copy()
+        pokemon_info = session().get(pokemons)
+        pokemon["name"] = pokemon_info.html.find(".mini", first=True).text.split("\n")[0]
+        pokemons_dictionary.append(pokemon)
+
+    return pokemons_dictionary
+
+
+def main():
+    url_list = url()
+    pokemons = get_pokemon(url_list)
+    print(pokemons)
+    # AGREGAR AL DICCIONARIO EL TIPO DE POKEMON QUE ES
+
+
+if __name__ == '__main__':
+    main()
